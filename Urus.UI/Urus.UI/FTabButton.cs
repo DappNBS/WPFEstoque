f35888037f3916475ae603be2b526061b55a4680
lamborcai <lamborcai@gmail.com>
2019-03-05 18:15:49 +0800

    urus library

--- Urus.UI/Urus.UI/FTabButton.cs
+++ Urus.UI/Urus.UI/FTabButton.cs
@@ -1,8 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
 
 
 /**
@@ -20,7 +19,48 @@ using System.Threading.Tasks;
  */
 namespace UrusUI
 {
-    class FTabButton
+    public class FTabButton : Button
     {
+        static FTabButton()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(FTabButton), new FrameworkPropertyMetadata(typeof(FTabButton)));
+        }
+
+        #region 依赖属性
+        /// <summary>
+        /// Orientation 控制Tab icon 和 text 是否在一行
+        /// </summary>
+        public static readonly DependencyProperty OrientationProperty =
+            DependencyProperty.Register("TabOrientation", typeof(Orientation), typeof(FTabButton),
+                new FrameworkPropertyMetadata(Orientation.Vertical,FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public Orientation TabOrientation
+        {
+            get { return (Orientation)GetValue(OrientationProperty); }
+            set { SetValue(OrientationProperty, value); }
+        }
+
+        public static readonly DependencyProperty FIconProperty =
+            DependencyProperty.Register("icon", typeof(string), typeof(FTabButton),
+                new FrameworkPropertyMetadata("\ue604"));
+        public string Icon
+        {
+            get { return (string)GetValue(FIconProperty); }
+            set { SetValue(FIconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconSizeProperty =
+            DependencyProperty.Register("IconSize", typeof(int), typeof(FTabButton),
+                new FrameworkPropertyMetadata(20, FrameworkPropertyMetadataOptions.AffectsRender));
+
+        public int IconSize
+        {
+            get { return (int)GetValue(IconSizeProperty); }
+            set { SetValue(IconSizeProperty, value); }
+        }
+
+
+
+        #endregion
     }
 }
